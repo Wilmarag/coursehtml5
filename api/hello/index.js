967f48507839479f88dbc2a5b651f5db57f97644
Wilmar Agudelo <74171773+Wilmarag@users.noreply.github.com>
2026-04-21 23:09:27 -0500

    Update index.js

--- api/hello/index.js
+++ api/hello/index.js
@@ -1,1 +1,10 @@
 
+module.exports = async function (context, req) {
+    context.res = {
+        status: 200,
+        body: {
+            mensaje: "Hola desde el backend 🚀",
+            hora: new Date().toISOString()
+        }
+    };
+};
