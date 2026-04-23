86c709f52c9ff4f15ea8dd3ebdf74e9808311a86
Wilmar Agudelo <74171773+Wilmarag@users.noreply.github.com>
2026-04-22 22:02:27 -0500

    Update index.js

--- api/listarUsuarios/index.js
+++ api/listarUsuarios/index.js
@@ -1,1 +1,27 @@
 
+const sql = require('mssql');
+
+module.exports = async function (context, req) {
+
+    try {
+
+        await sql.connect(process.env.SQL_CONNECTION_STRING);
+
+        const resultado = await sql.query`
+            SELECT * FROM Usuarios
+            ORDER BY FechaRegistro DESC
+        `;
+
+        context.res = {
+            status: 200,
+            body: resultado.recordset
+        };
+
+    } catch (err) {
+
+        context.res = {
+            status: 500,
+            body: err.message
+        };
+    }
+};
