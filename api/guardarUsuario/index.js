5a38fe970f5f816ed4cb646398d4900c57bdb80b
Wilmar Agudelo <74171773+Wilmarag@users.noreply.github.com>
2026-04-22 20:39:27 -0500

    Update index.js

--- api/guardarUsuario/index.js
+++ api/guardarUsuario/index.js
@@ -1,1 +1,29 @@
 
+const sql = require('mssql');
+
+module.exports = async function (context, req) {
+
+    const { nombre, email } = req.body;
+
+    try {
+
+        await sql.connect(process.env.SQL_CONNECTION_STRING);
+
+        await sql.query`
+            INSERT INTO Usuarios (Nombre, Email)
+            VALUES (${nombre}, ${email})
+        `;
+
+        context.res = {
+            status: 200,
+            body: "Usuario guardado"
+        };
+
+    } catch (err) {
+
+        context.res = {
+            status: 500,
+            body: err.message
+        };
+    }
+};
